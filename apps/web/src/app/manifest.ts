915b4968e1c91d30c191a8ab2976ac4e3dad2cd1
Mohamed-El-Hussainy <mohamed.noda.b2@gmail.com>
2026-04-04 17:41:39 +0200

    services

--- apps/web/src/app/manifest.ts
+++ apps/web/src/app/manifest.ts
@@ -19,7 +19,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/icon-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/icon-512x512.png",
